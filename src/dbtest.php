4136f9c1bc46bec378c368f530b344187ff2bd17
anandka2000 <anand.agra2@gmail.com>
2020-07-31 13:06:11 -0400

    fixed display

--- src/dbtest.php
+++ src/dbtest.php
@@ -3,14 +3,19 @@ $dbhost = getenv ("MYSQL_SERVICE_HOST");
 $dbport = getenv ("MYSQL_SERVICE_PORT");
 $dbuser = 'root';
 $dbpwd  = getenv ("MYSQL_ROOT_PASSWORD");
-$dbpwd  = getenv ("MYSQL_DATABASE");
+$dbname = getenv ("MYSQL_DATABASE");
+
+echo "dbhost is: ".$dbhost."<p>\n";
+echo "dbport is: ".$dbport."<p>\n";
+echo "dbname is: ".$dbname."<p>\n";
+
 
 $connection = mysql_connect($dbhost.":".$dbport, $dbuser, $dbpwd);
 
 if (!$connection) {
         echo "could not connect to database";
 } else {
-        echo "connected to database. <br>" ;
+        echo "connected to database.<p>\n" ;
 }
 
 $dbconnection = mysql_select_db ($dbname);
@@ -19,7 +24,7 @@ $query = "SELECT * from users";
 
 $rs = mysql_query($query);
 while ($row = mysql_fetch_assoc($rs)) {
-        echo $row['user_id']." ".$row['username']."\n";
+        echo $row['user_id']." ".$row['username']."<p>\n";
 }
-
+echo "<p>\n";
 mysql_close ();
